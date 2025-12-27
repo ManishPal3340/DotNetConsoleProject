f98ae3f13f5ebba1e583c59bd31dda98a7b2f9e3
Manish Pal <manishpal3340@gmail.com>
2025-12-27 15:19:06 +0530

    Add files via upload

---
+++ InheritnaceExample.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Vehicle  // Parent Class
+{
+    protected string brand;
+    protected int speed;
+
+
+    public void SetVehicleDetails(string brandName, int maxSpeed)
+    {
+        brand = brandName;
+        speed = maxSpeed;
+    }
+    public void ShowDetails()
+    {
+        Console.WriteLine("Brand: " + brand);
+        Console.WriteLine("Max Speed: " + speed + " km/h");
+
+    }
+}
+
+class Car: Vehicle
+{
+    public void CarType()
+    {
+        Console.WriteLine("This is a Car");
+    }
+}
+
+class InheritanceExample
+{
+    public static void Main(string[] args)
+    {
+        Car car = new Car();
+
+        car.SetVehicleDetails("Honda", 180);
+        car.CarType();
+        car.ShowDetails();
+
+    } 
+}
