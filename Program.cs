8f08835f3f385d70a40065a7dc1c060071057fbc
Manish Pal <manishpal3340@gmail.com>
2025-12-27 14:13:53 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Student
+{
+    public string Name;
+    public int Age;
+
+    public void Display()// values Display
+    {
+        Console.WriteLine("Name : "+ Name);
+        Console.WriteLine("Age : "+ Age );
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        // create aobject
+        Student s1=new Student();
+        s1.Name = "Manish";
+        s1.Age = 20;
+        s1.Display();
+    }
+}
