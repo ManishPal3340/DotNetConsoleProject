73c140ddf4d92ef1f1b3130dac15ca56a06f5dc0
Manish Pal <manishpal3340@gmail.com>
2025-12-27 14:14:30 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,24 +1,106 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
-class Student
+class BankAccount
 {
-    public string Name;
-    public int Age;
+    private string accHoldName;
+   // private string accountName;
+   // private string accountType;
+    private string accountPassword;
+    private string accHoldEmail;
+    private string accHoldAddress;
+    private double balance;
 
-    public void Display()// values Display
+
+    public void SetName(string name)
     {
-        Console.WriteLine("Name : "+ Name);
-        Console.WriteLine("Age : "+ Age );
+        accHoldName = name; 
     }
+    public string GetName()
+    {
+        return accHoldName;
+    }
+    public void SetPassword(string password) 
+    {
+        accountPassword = password;
+
+    }
+    public string SetPassword()
+    {
+        return accountPassword;
+    }
+
+    public void SetEmail(string email)
+    {
+        accHoldEmail = email;
+
+    }
+    public string GetEmail()
+    {
+        return accHoldEmail;
+    }
+
+    public void SetAddress(string address)
+    {
+        accHoldAddress=address;
+    }
+    public string GetAddress()
+    {
+        return accHoldAddress;
+
+    }
+
+    public void Deposite(double amount)
+    {
+        if (amount > 0)
+        {
+            balance += amount;
+            Console.WriteLine("Amount Deposite :" + amount);
+        }
+        else
+        {
+            Console.WriteLine("Invalid Deposite");
+        }
+    }
+
+    public void Withdrow(double amount)
+    {
+
+        if (amount > 0 && amount <= balance)
+        {
+            balance -= amount;
+            Console.WriteLine("Amount Withdrow :" + amount);
+        }
+        else
+        {
+            Console.WriteLine("Insufficient Balance");
+        }
+
+        
+    }
+    public double GetBalance()
+    {
+        return balance; 
+    }
+
 }
 class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
+
+       
     {
-        // create aobject
-        Student s1=new Student();
-        s1.Name = "Manish";
-        s1.Age = 20;
-        s1.Display();
+        BankAccount ac=new BankAccount();
+        ac.SetName("Manish");
+        ac.SetPassword("12345");
+        ac.SetEmail("manish@gmail.com");
+        ac.Deposite(100);
+        ac.Withdrow(10);
+
+        Console.WriteLine("Account Holder: " + ac.GetName());
+        Console.WriteLine("Current Balance: " + ac.GetBalance());
+        Console.WriteLine("Account Password: " + ac.SetPassword());
+        Console.WriteLine("holder Email ID: " + ac.GetAddress());
+        
     }
 }
