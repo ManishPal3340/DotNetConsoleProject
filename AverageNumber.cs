fcf5d97c9db54b5727c4d5a8a29d4a67c4c6fbc9
Manish Pal <manishpal3340@gmail.com>
2025-12-29 11:40:07 +0530

    Add files via upload

---
+++ AverageNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class AverageNumber
+{
+    public static void  Main(string[] args)
+    {
+        Console.WriteLine("Enter tNE Number");
+
+        int a=Convert.ToInt32(Console.ReadLine())   ;
+
+        int[]marks=new int[a];
+        int total=0 ;
+
+
+        for(int i = 0; i < a; i++)
+        {
+            Console.WriteLine ("Enter Marks " +(i+1) + " :");
+            //marks[i]=Convert.ToInt32 (Console.ReadLine());
+            total += marks[i];
+        }
+        double aver = (double)total / a;
+        Console.WriteLine("Total Marks: " + total);
+        Console.WriteLine("Average: " + aver);
+
+
+    }
+}
