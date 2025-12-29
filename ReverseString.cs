180fb133166ada42709a73784b48d2e9d525f871
Manish Pal <manishpal3340@gmail.com>
2025-12-29 12:06:50 +0530

    Add files via upload

---
+++ ReverseString.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class ReverseString
+{
+    public static void  Main(string[] args)
+    {
+        System.Console.WriteLine("Enter The String");
+        string input=Console.ReadLine();
+
+        string reverse = "";
+        for (int i = input.Length - 1; i > 0; i --)
+        {
+            reverse=reverse+input[i];
+        }
+        Console.WriteLine("Reversed String" + reverse);
+    }
+}
