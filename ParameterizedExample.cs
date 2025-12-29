2420e4f25d99803128b88510ed8d9cca32dcb570
Manish Pal <manishpal3340@gmail.com>
2025-12-29 22:27:02 +0530

    Add files via upload

---
+++ ParameterizedExample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+class Student
+{
+    public string Name;
+    public int Age;
+
+
+    public Student(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public void show()
+    {
+        Console.WriteLine("Name : " + Name);
+        Console.WriteLine("Age : " + Age);
+    }
+}
+
+class ParameterizedExample
+{
+    public static void Main(string[] args)
+    {
+        Student s1 = new Student("Manish", 22);
+        s1.show();
+    }
+}
---
+++ StaticVariable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Company
+{
+    public static string CompanyName = "ABC Pvt Ltd";
+
+    public static void Display()
+    {
+        Console.WriteLine("Company Name: " + CompanyName);
+    }
+}
+
+class StaticVariable
+{
+    static void Main()
+    {
+        Company.Display();
+    }
+}
