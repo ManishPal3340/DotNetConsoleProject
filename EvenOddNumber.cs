3ae7a402b28c90ab38954e7c725fc8c6a6803d67
Manish Pal <manishpal3340@gmail.com>
2025-12-29 11:19:05 +0530

    Add files via upload

---
+++ EvenOddNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+ class EvenOddNumber
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Number");
+        int a=Convert.ToInt32(Console.ReadLine())   ;
+
+        if (a %2==0)
+        {
+
+            Console.WriteLine("This is Even Number");
+        }
+        else
+        {
+            Console.WriteLine("This is Odd Number");
+        }
+    }
+}
