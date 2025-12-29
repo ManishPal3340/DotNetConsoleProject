13c76391c77e72ad64af277b252886a1167a8e41
Manish Pal <manishpal3340@gmail.com>
2025-12-29 12:05:12 +0530

    Add files via upload

---
+++ LargestNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class LargestNumber
+{
+    public static void Main(string[] args)
+    {
+
+        int[] arr = { 12, 11, 4, 4, 5, 6, 7, 32, 4, 4, 45,45 };
+        int max = arr[0];
+
+
+        for (int i=1;i<arr.Length;i++)
+        {
+            if(arr[i] > max)
+                max=arr[i];
+        }
+
+
+
+
+
+
+        Console.WriteLine(" the Largest  Number" + max);
+    }
+}
