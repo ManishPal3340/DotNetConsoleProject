5908f5201b6a3611759fa7dd2786887808075ea3
Manish Pal <manishpal3340@gmail.com>
2025-12-30 11:42:06 +0530

    Add files via upload

---
+++ CollectionUsingListExample.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+class CollectionUsingListExample
+{
+    public static void Main(string[] args)
+    {
+        List<int> marks = new List<int>();
+        Console.Write("how many Marks ?");
+        int n=Convert.ToInt32(Console.ReadLine());
+        for (int i=0;i<n;i++)
+        {
+            Console.Write("Enter Marks :");
+            marks.Add(Convert.ToInt32(Console.ReadLine()));
+
+        }
+        Console.WriteLine("Marks:");
+        foreach (int m in marks)
+        {
+            Console.WriteLine(m); 
+        }
+    }
+}
+
+
---
+++ DictionaryList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<int, string> students = new Dictionary<int, string>();
+
+        students.Add(1, "Manish");
+        students.Add(2, "Rahul");
+
+        foreach (var s in students)
+        {
+            Console.WriteLine(s.Key + " : " + s.Value);
+        }
+    }
+}
+
+
+//DictionaryList
+
+
+
