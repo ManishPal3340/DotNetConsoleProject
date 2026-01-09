06a7b84b6ae2f1360b35213b20be0b3f933bd102
Manish Pal <manishpal3340@gmail.com>
2026-01-09 17:49:58 +0530

    Add files via upload

---
+++ Day12First.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+class Day12First
+{
+    public static void Main(string[] args)
+    {
+        List<int> number = new List<int>()
+        {
+            1,3,4,6,7,8,4,9,9,0,64,3,2,5,2,56,7,8,6,4,3,36,7,8,76
+        };
+
+        var result = number.Where(n => n > 10);
+        foreach(var n in result )
+            Console.WriteLine(n);
+    }
+}
---
+++ secondSelectDay12.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Globalization;
+class secondSelectDay12
+{
+    public static void Main(string[] args)
+    {
+        var name = new List<string>()
+        {
+            "ram","Shyam","Mohan"
+        };
+        // TO UPPERCASE 
+        var upperNames= name.Select(n=>n.ToUpper()) ;
+
+        foreach (var n in upperNames)
+        {
+            Console.WriteLine("UPPERCASE" + n);
+
+          
+        }
+        Console.WriteLine("====================================");
+
+        // lowerCase 
+        var name2 = new List<string>()
+        {
+            "Manish ","Shyam", "YASH"
+        };
+        var lowerNames = name2.Select(n1 => n1.ToLower());
+        foreach (var n1 in lowerNames) 
+            { 
+            Console.WriteLine("lowerCase" + n1);
+           
+        }
+
+        Console.WriteLine("\n====================================");
+
+        // orderBy & OrderDecending
+
+        var name3 = new List<string>()
+        {
+            "Shriya", "NEHA","Ankita"
+
+
+        };
+        //var name4 = new List<string>()
+        var sorted = name3.OrderBy(n2 => n2);
+
+            foreach (var n3 in sorted)
+        {
+            Console.WriteLine("sorted" + n3);
+
+           
+        }
+        Console.WriteLine("\n====================================");
+
+
+
+        // DECendiong order
+        var name4 = new List<string>()
+            {
+                 "Shriya", "NEHA","Ankita","Manish ","Shyam", "YASH"
+            };
+
+        var desc=name4.OrderByDescending(n4 => n4);
+
+        foreach (var n4 in desc)
+        {
+            Console.WriteLine(" DECendiong order" + n4);
+
+           
+
+        }
+        Console.WriteLine("\n====================================");
+
+        // Assending Order
+
+        var name5 = new List<string>()
+        {
+             "Shriya", "NEHA","Ankita","Manish ","Shyam", "YASH"
+        };
+
+        var asc = name4.OrderBy(n5 => n5);
+
+        foreach (var n5 in asc)
+        {
+            Console.WriteLine("Assending Order" + n5);
+           
+
+        }
+        Console.WriteLine("\n====================================");
+
+
+        // REmove Data
+
+        var name6 = new List<string>()
+        {
+             "Shriya", "NEHA","Ankita","Manish ","Shyam", "YASH"
+        };
+
+
+       name6.RemoveAll(n=>n.StartsWith("S"));
+
+        foreach (var n6 in name6)
+        {
+            Console.WriteLine("REmove Data" + n6);
+           
+
+
+        }
+        Console.WriteLine("\n====================================");
+
+        // Sinlge Item Remove
+        var name7 = new List<string>()
+        {
+             "Shriya", "NEHA","Ankita","Manish ","Shyam", "YASH"
+        };
+
+        name7.Remove("Shriya");
+        foreach (var n7 in name7)
+        {
+            Console.WriteLine("Sinlge Item Remove" + n7);
+          
+        }
+        Console.WriteLine("\n====================================");
+
+
+        // RemoveAt(index)
+
+        var name8 = new List<string>()
+        {
+            "Shriya", "NEHA","Ankita","Manish ","Shyam", "YASH"
+        };
+        name8.RemoveAt(3);
+
+        foreach (var n8 in name8)
+        {
+            Console.WriteLine("RemoveAt(index" + n8);
+
+          
+
+        }
+        Console.WriteLine("\n====================================");
+
+        // REmove Range
+
+        var name9 = new List<string>()
+        {
+             "Shriya", "NEHA","Ankita","Manish ","Shyam", "YASH"
+        };
+
+        name9.RemoveRange(1, 4);
+
+        foreach (var n9 in name9)
+        {
+            Console.WriteLine("RemoveRange" + n9);
+
+           
+
+        }
+        Console.WriteLine("\n====================================");
+
+    }
+}
