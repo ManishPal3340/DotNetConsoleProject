b9237c01334fdf63c19c602786f67a42a98b3bd4
Manish Pal <manishpal3340@gmail.com>
2026-01-11 15:13:32 +0530

    Add files via upload

---
+++ ThirdInterviewQuetion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Test
+{
+    public const int A = 10;
+    public readonly int B;
+
+    public Test()
+    {
+        B = 20;
+    }
+}
+
+class ThirdInterviewQuetion
+{
+    static void Main()
+    {
+        Test t = new Test();
+        Console.WriteLine(Test.A);
+        Console.WriteLine(t.B);
+    }
+}
---
+++ firstInterviewQuetion.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+// Abstract class
+abstract class Animal
+{
+    public abstract void Sound();
+
+    public void Sleep()
+    {
+        Console.WriteLine("Animal is sleeping");
+    }
+}
+
+// Interface
+interface IBird
+{
+    void Fly();
+}
+
+// Class using both
+class Dog : Animal, IBird
+{
+    public override void Sound()
+    {
+        Console.WriteLine("Dog barks");
+    }
+
+    public void Fly()
+    {
+        Console.WriteLine("Bird can fly");
+    }
+}
+
+class firstInterviewQuetion
+{
+    static void Main()
+    {
+        Dog d = new Dog();
+        d.Sound();
+        d.Sleep();
+        d.Fly();
+    }
+}
